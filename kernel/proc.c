a1aad186ca6079b5cc4f6611a381fe34264d60f5
liuCanjia <981786891@qq.com>
2022-05-30 21:43:03 +0800

    lab3: Speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,15 @@ found:
     return 0;
   }
 
+#ifdef LAB_PGTBL
+  if((p->ugetpidframe = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->ugetpidframe->pid = p->pid;
+#endif
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +162,11 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+#ifdef LAB_PGTBL
+  if(p->ugetpidframe)
+    kfree((void*)p->ugetpidframe);
+  p->ugetpidframe = 0;
+#endif
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -196,6 +210,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+#ifdef LAB_PGTBL
+  // map the USYSCALL just below TRAPFRAME (for speed up ugetpid())
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->ugetpidframe), PTE_U | PTE_R) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+#endif
   return pagetable;
 }
 
@@ -206,6 +229,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+#ifdef LAB_PGTBL
+  uvmunmap(pagetable, USYSCALL, 1, 0);
+#endif
   uvmfree(pagetable, sz);
 }
 
