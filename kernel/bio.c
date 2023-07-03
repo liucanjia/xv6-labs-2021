42ff0a40e01ad4d8d6b8007e91c94efbdcacbd67
liuCanjia <981786891@qq.com>
2023-07-04 00:39:02 +0800

    lab8: Buffer cache

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,7 +23,21 @@
 #include "fs.h"
 #include "buf.h"
 
+#ifdef LAB_LOCK
+#define BUCKETSZ 13
+#define LOCKNAMESIZE 32
+#define HASH(blockno) (blockno % BUCKETSZ)
+
+extern uint ticks;
+#endif
+
 struct {
+#ifdef LAB_LOCK
+  struct spinlock hashLock;     //搜索哈希表时的全局锁
+  struct buf buf[NBUF];
+  struct buf bufBucket[BUCKETSZ];
+  struct spinlock bucketLock[BUCKETSZ];
+#else
   struct spinlock lock;
   struct buf buf[NBUF];
 
@@ -31,11 +45,38 @@ struct {
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
   struct buf head;
+#endif
 } bcache;
 
 void
 binit(void)
 {
+#ifdef LAB_LOCK
+  char lockName[LOCKNAMESIZE];
+  int n;
+
+  initlock(&bcache.hashLock, "bcacheHashLock");
+
+  for(int i = 0; i < BUCKETSZ; i++) {
+    n = snprintf(lockName, LOCKNAMESIZE, "bcacheBucket%d", i);
+    lockName[n] = 0;
+    initlock(&bcache.bucketLock[i], lockName);
+  }
+
+  for(int i = 0; i < BUCKETSZ; i++) {
+    bcache.bufBucket[i].prev = &bcache.bufBucket[i];
+    bcache.bufBucket[i].next = &bcache.bufBucket[i];
+  }
+
+  for(struct buf *b = bcache.buf; b < bcache.buf+NBUF; b++) {
+    b->next = bcache.bufBucket[0].next;
+    b->prev = &bcache.bufBucket[0];
+    bcache.bufBucket[0].next->prev = b;
+    bcache.bufBucket[0].next = b;
+    initsleeplock(&b->lock, "buffer");
+    b->timeStamp = ticks;
+  }
+#else
   struct buf *b;
 
   initlock(&bcache.lock, "bcache");
@@ -50,6 +91,7 @@ binit(void)
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
+#endif
 }
 
 // Look through buffer cache for block on device dev.
@@ -58,6 +100,86 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
+#ifdef LAB_LOCK
+  struct buf *b;
+  int bucketIdx = HASH(blockno);
+
+  acquire(&bcache.bucketLock[bucketIdx]);
+
+  for(b = bcache.bufBucket[bucketIdx].next; b != &bcache.bufBucket[bucketIdx]; b = b->next) {
+    if(b->dev == dev && b->blockno == blockno) {
+      b->refcnt++;
+      release(&bcache.bucketLock[bucketIdx]);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+
+  // Not cached.
+  release(&bcache.bucketLock[bucketIdx]);
+  acquire(&bcache.hashLock);
+
+  // 两个进程同时查找相同的dev, blockno. 第二个进程进到遍历时, 已经分配过buf了, 故再次查找是否buf已存在
+  acquire(&bcache.bucketLock[bucketIdx]);
+
+  for(b = bcache.bufBucket[bucketIdx].next; b != &bcache.bufBucket[bucketIdx]; b = b->next) {
+    if(b->dev == dev && b->blockno == blockno) {
+      b->refcnt++;
+      release(&bcache.bucketLock[bucketIdx]);
+      release(&bcache.hashLock);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+
+  release(&bcache.bucketLock[bucketIdx]);
+  
+  // 确实未分配, 则寻找可被驱逐的buf进行分配
+  struct buf *replaceBuf = 0;
+  uint minTimeStamp = ticks;
+
+  // 遍历哈希表
+  for(int i = 0; i < BUCKETSZ; i++) {
+    acquire(&bcache.bucketLock[i]);
+
+    for(b = bcache.bufBucket[i].next; b != &bcache.bufBucket[i]; b = b->next) {
+      if(b->refcnt == 0 && minTimeStamp >= b->timeStamp) {
+        replaceBuf = b;
+        minTimeStamp = b->timeStamp;
+      }
+    }
+
+    if(replaceBuf) {
+      replaceBuf->dev = dev;
+      replaceBuf->blockno = blockno;
+      replaceBuf->valid = 0;
+      replaceBuf->refcnt = 1;
+
+      if(i != bucketIdx) {
+        replaceBuf->next->prev = replaceBuf->prev;
+        replaceBuf->prev->next = replaceBuf->next;
+        release(&bcache.bucketLock[i]);
+
+        acquire(&bcache.bucketLock[bucketIdx]);
+        replaceBuf->next = bcache.bufBucket[bucketIdx].next;
+        replaceBuf->prev = &bcache.bufBucket[bucketIdx];
+        bcache.bufBucket[bucketIdx].next->prev = replaceBuf;
+        bcache.bufBucket[bucketIdx].next = replaceBuf;
+        release(&bcache.bucketLock[bucketIdx]);
+      } else {
+        release(&bcache.bucketLock[i]);
+      }
+
+      release(&bcache.hashLock);
+      acquiresleep(&replaceBuf->lock);
+      return replaceBuf;
+    } 
+
+    release(&bcache.bucketLock[i]);
+  }
+
+  release(&bcache.hashLock);
+#else
   struct buf *b;
 
   acquire(&bcache.lock);
@@ -85,6 +207,7 @@ bget(uint dev, uint blockno)
       return b;
     }
   }
+#endif
   panic("bget: no buffers");
 }
 
@@ -121,6 +244,16 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
+#ifdef LAB_LOCK
+  int bucketIdx = HASH(b->blockno);
+
+  acquire(&bcache.bucketLock[bucketIdx]);
+  b->refcnt--;
+  if (b->refcnt == 0) {
+    b->timeStamp = ticks;
+  }
+  release(&bcache.bucketLock[bucketIdx]);
+#else
   acquire(&bcache.lock);
   b->refcnt--;
   if (b->refcnt == 0) {
@@ -134,20 +267,37 @@ brelse(struct buf *b)
   }
   
   release(&bcache.lock);
+#endif
 }
 
 void
 bpin(struct buf *b) {
+#ifdef LAB_LOCK
+  int bucketIdx = HASH(b->blockno);
+
+  acquire(&bcache.bucketLock[bucketIdx]);
+  b->refcnt++;
+  release(&bcache.bucketLock[bucketIdx]);
+#else
   acquire(&bcache.lock);
   b->refcnt++;
   release(&bcache.lock);
+#endif
 }
 
 void
 bunpin(struct buf *b) {
+#ifdef LAB_LOCK
+  int bucketIdx = HASH(b->blockno);
+
+  acquire(&bcache.bucketLock[bucketIdx]);
+  b->refcnt--;
+  release(&bcache.bucketLock[bucketIdx]);
+#else
   acquire(&bcache.lock);
   b->refcnt--;
   release(&bcache.lock);
+#endif
 }
 
 
--- kernel/param.h
+++ kernel/param.h
@@ -9,5 +9,9 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+#ifdef LAB_LOCK
+#define FSSIZE       10000  // size of file system in blocks
+#else
 #define FSSIZE       1000  // size of file system in blocks
+#endif
 #define MAXPATH      128   // maximum file path name
